b3d16c46ba1fc82660faa21e8b3e08001b540f4f
Larissa Strauch <lstrauch@mail.uni-mannheim.de>
2018-03-20 13:47:58 +0100

    #10  first commit

---
+++ src/gui/MainClass.java
@@ -0,0 +1,57 @@
+package gui;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class MainClass extends Application {
+
+	private Stage primaryStage;
+	private AnchorPane root;
+
+	public Stage getStage() {
+		return primaryStage;
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			this.primaryStage = primaryStage;
+			mainWindow();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public AnchorPane getAnchorPane() {
+		return root;
+	}
+
+	public void mainWindow() {
+		try {
+			FXMLLoader loader = new FXMLLoader(MainClass.class.getResource("Start.fxml"));
+			root = loader.load();
+
+			StartController startCon = new StartController();
+			startCon.setMain(this);
+
+			Scene scene = new Scene(root);
+			// scene.getStylesheets().add(getClass().getResource("gui.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/gui/StartController.java
@@ -0,0 +1,57 @@
+package gui;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class StartController {
+
+	public MainClass main;
+	@FXML
+	private TextField nutzername;
+	@FXML
+	private Button neuanmelden;
+	@FXML
+	private AnchorPane root;
+	@FXML
+	private HBox hbox;
+
+	@FXML
+	public void submit() {
+		// Nutzernamen an Logik weitergeben
+		// Spielerprofil laden
+		Stage newStage = new Stage();
+		try {
+			root = (AnchorPane) FXMLLoader.load(getClass().getResource("Spielmodi.fxml"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Scene scene = new Scene(root);
+		newStage.setScene(scene);
+		newStage.setTitle("Spielmodi");
+		newStage.showAndWait();
+		System.exit(0);
+	}
+
+	@FXML
+	public void neuerAccount() {
+		// Settingsfeld für neuen Benutzernamen anzeigen
+		nutzername.setPromptText("Neuer Nutzername");
+		nutzername.setPrefHeight(71);
+		hbox.getChildren().remove(neuanmelden);
+	}
+
+	public void setMain(MainClass main2) {
+		// TODO Auto-generated method stub
+		this.main = main2;
+
+	}
+}
