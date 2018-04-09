074d85b3815a477f5f4b255affac5abf8a286d18
Larissa Strauch <lstrauch@mail.uni-mannheim.de>
2018-04-09 17:28:44 +0200

    #39 ...

--- src/interfaces/GuiLogic.java
+++ src/interfaces/GuiLogic.java
@@ -1,6 +1,14 @@
 package interfaces;
 
+import javafx.scene.image.Image;
+
 //GUI to Logic, implemented by Logic
 public interface GuiLogic {
+
+	// Bilder der Karten erhalten
+	public Image[] getCards();
+
+	// Checken ob Karte gespielt werden darf
+	public boolean abletoPlay();
 
 }
