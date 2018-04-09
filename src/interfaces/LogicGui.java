7c36012f5d8aa1daab4b933ae89838bab161ab0e
Larissa Strauch <lstrauch@mail.uni-mannheim.de>
2018-04-09 17:38:03 +0200

    #39 gehts endlich?

--- src/interfaces/LogicGui.java
+++ src/interfaces/LogicGui.java
@@ -1,14 +1,6 @@
 package interfaces;
 
-import javafx.scene.image.Image;
-
 //Logic to GUI, implemented by GUI
 public interface LogicGui {
-
-	// Bilder der Karten erhalten
-	public Image[] getCards();
-
-	// Checken ob Karte gespielt werden darf
-	public boolean abletoPlay();
 
 }
