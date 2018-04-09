b248934cf34d7a534dbb1a3e89add8e63a328135
Larissa Strauch <lstrauch@mail.uni-mannheim.de>
2018-04-09 16:22:40 +0200

    #39 Karten sind anklickbar und werden auf das Stichblatt gelegt

--- src/interfaces/LogicGui.java
+++ src/interfaces/LogicGui.java
@@ -1,6 +1,11 @@
 package interfaces;
 
+import javafx.scene.image.Image;
+
 //Logic to GUI, implemented by GUI
 public interface LogicGui {
+
+	// Bilder der Karten erhalten
+	public Image[] getCards();
 
 }
