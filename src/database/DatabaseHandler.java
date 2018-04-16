88b15c4a2a09a69904c6ba5ac3b48920fefb21f2
Duygu Pervane <dpervane@mail.uni-mannheim.de>
2018-04-16 19:42:10 +0200

    #11 update database 2

--- src/database/DatabaseHandler.java
+++ src/database/DatabaseHandler.java
@@ -3,12 +3,8 @@ package database;
 import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import logic.Player;
-import logic.Card;
-
-
 
 public class DatabaseHandler extends database {
   
@@ -28,39 +24,31 @@ public class DatabaseHandler extends database {
   public void prepareStatements() {
     try {
         
-        insPlayer = c.prepareStatement("INSERT INTO Player (id, name, nickname, score, profilePicture) VALUES (?,'?','?',?,?);");
-        
-        
-        selectPlayer = c.prepareStatement("SELECT * FROM Player WHERE (nickname LIKE '%?%') ORDER BY nickname;");
+        insPlayer = c.prepareStatement("INSERT INTO Player (id, name, score, profilePicture) VALUES (?,'?','?,?);");
+              
+        selectPlayer = c.prepareStatement("SELECT * FROM Player WHERE (name LIKE '%?%') ORDER BY name;");
         
         selectCard = c.prepareStatement("SELECT * FROM Cards WHERE (colour LIKE '%?%') AND (number LIKE '%?%'");
                 
         deletePlayer = c.prepareStatement("DELETE FROM Player WHERE id =?;");        
         
-        editPlayer = c.prepareStatement(
-            "UPDATE Kontakte SET name = '?', nickname = '?', score = ?, profilePicture = ? WHERE name LIKE = '%?%';");
+        editPlayer = c.prepareStatement("UPDATE Kontakte SET name = '?', score = ?, profilePicture = ? WHERE name LIKE = '%?%';");
     }
     catch(SQLException e) {
       e.printStackTrace();
     }
-  }
-  
-//  insertPlayer: Hier soll ein als Parameter übergebener Spieler in die Datenbank eingetragen werden. 
+  }  
   
   public void insertPlayer(Player player) {
     
     try {
         insPlayer.setString(1, player.getName());
-        //insPlayer.setString(2, player.getNickname());
-        //insPlayer.setInt(3, player.getScore());
-        //insPlayer.setBlob(4, player.getProfilePicture());
         insPlayer.executeUpdate();
 
     } catch (SQLException e) {    
         e.printStackTrace();
     }
 }
-  //selectPlayer: Hier soll der der Spieler aus der Datenbank ausgewählt werden
   
   public void selectPlayer(Player player) {
     try {
@@ -69,9 +57,7 @@ public class DatabaseHandler extends database {
     }catch (SQLException e) {
       e.printStackTrace();
     }
-  }
-  
-//deletePlayer: Hier soll der übergebene Spieler aus der Datenbank gelöscht werden.
+  } 
   
   public void deletePlayer(Player player) {
     
@@ -81,57 +67,29 @@ public class DatabaseHandler extends database {
     } catch (SQLException e) {       
         e.printStackTrace();
     }
-  }t werden. 
+  }
   
-  public void editPlayer(Player original, Player neu) {
-    
+  public void editPlayer(Player original, Player neu) {    
     try {
-        editPlayer.setString(1, neu.getName());
-        //editPlayer.setString(2, neu.getNickname());
-        //editPlayer.setInt(3, neu.getScore());
-        //editPlayer.setBlob(4, neu.getProfilePicture());      
-      
+        editPlayer.setString(1, neu.getName());         
         editPlayer.executeUpdate();
 }
     catch (SQLException e) {       
       e.printStackTrace();
     }
   }
+
   
-  public List<Card> getCard(String type, int value, Blob image ) {
-
-    List<Card> ausg = new ArrayList<Card>();
-
+  public Blob getCard(Blob card) {
     try {
-        String typehilf;
-        int id, valuehilf;
-        Blob imagehilf;
-        Card hilf;
-        
-        selectCard.setString(1, type);
-        selectCard.setInt(2, value);
-        selectCard.setBlob(3, image);
-        selectCard.executeUpdate();
-        ResultSet rs = selectCard.executeQuery();
-        while (rs.next()) {
-            id = rs.getInt("id"); 
-            valuehilf = rs.getInt("value");
-            imagehilf= rs.getBlob("image");
-            typehilf = rs.getString("type");            
-            //hilf = new Card(id, valuehilf, imagehilf, typehilf);
-           //ausg.add(hilf);
-        }
-        rs.close();
-
-    } catch (SQLException e) {
-        e.printStackTrace();
+      selectCard.setBlob(1, card.getBinaryStream());
+      selectCard.execute();
     }
-
-    return ausg;
+    catch (SQLException e) {       
+      e.printStackTrace();
+  }
+    return card;
 }
-        
-  
-  
-  
-
 }
+
+
