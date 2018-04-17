719ed4abab25bd05e722607674cf8d430589bb34
Larissa Strauch <lstrauch@mail.uni-mannheim.de>
2018-04-17 23:37:45 +0200

    #40 gefuellt

--- src/gui/ImplementsLogicGui.java
+++ src/gui/ImplementsLogicGui.java
@@ -8,41 +8,59 @@ import logic.Position;
 
 public class ImplementsLogicGui implements LogicGui {
 
-  @Override
-  public String startGui() {
-    // TODO Auto-generated method stub
-    GuiController guiCon = new GuiController();
-    return null;
-  }
+  private GuiController guiCon = new GuiController();
 
-  @Override
-  public boolean askForMultiplayer() {
-    // TODO Auto-generated method stub
+  // @Override
+  // public String startGui() {
+  // // TODO Auto-generated method stub
+  // GuiController guiCon = new GuiController();
+  // return null;
+  // }
 
-    return false;
-  }
+  // @Override
+  // public boolean askForMultiplayer() {
+  // // TODO Auto-generated method stub
+  //
+  // return false;
+  // }
 
   @Override
   public void openSinglePlayerLobby() {
     // TODO Auto-generated method stub
+    guiCon.displayLobbyLocal();
 
   }
 
   @Override
   public void openMultiPlayerLobby() {
     // TODO Auto-generated method stub
+    guiCon.displayLobbyOnline();
 
   }
 
   @Override
   public void setGameSettings(GameSettings gs) {
     // TODO Auto-generated method stub
+    guiCon.displaySettings();
 
   }
 
   @Override
   public void startPlay(ArrayList<Card> hand, Position position) {
     // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void startGui() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void decideGameMode() {
+    // TODO Auto-generated method stub
+    guiCon.displayChooseGame();
 
   }
 
