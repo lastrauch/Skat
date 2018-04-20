fc995ab5e71f090c4def5a49347cfe3329ed3145
Larissa Strauch <lstrauch@mail.uni-mannheim.de>
2018-04-20 14:03:55 +0200

    #37 puh

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,7 +6,6 @@ import javafx.application.Application;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
     Application.launch(GuiController.class, args);
   }
 
