0cb9e57403bf9ebfe7303ad642f24fa76fa9cae9
Duygu Pervane <dpervane@mail.uni-mannheim.de>
2018-04-23 20:44:48 +0200

    #11

--- src/database/DatabaseHandler.java
+++ src/database/DatabaseHandler.java
@@ -1,21 +1,28 @@
 package database;
 
+import java.awt.Image;
+import java.io.InputStream;
 import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import javax.imageio.ImageIO;
 import logic.Card;
 import logic.Player;
 
 
 public class DatabaseHandler extends database {
   
-  private static PreparedStatement insPlayer;
+  private static PreparedStatement insertPlayer;
   private static PreparedStatement selectPlayer;
   private static PreparedStatement selectCard;
+  private static PreparedStatement selectCardDarker;
   private static PreparedStatement deletePlayer;
-  private static PreparedStatement editPlayer;
+  private static PreparedStatement changeName;
+  private static PreparedStatement changeImage;
+  private static PreparedStatement checkPlayer;
+  private static PreparedStatement countPlayer;
   
   private Connection c = null;
   
@@ -28,16 +35,22 @@ public class DatabaseHandler extends database {
   public void prepareStatements() {
     try {
         
-        insPlayer = c.prepareStatement("INSERT INTO Player (id, name, score, profilePicture) VALUES (?,'?',?,?);");    
+        insertPlayer = c.prepareStatement("INSERT INTO Player (id, name, score, profilePicture) VALUES (?,'?',?,?);");    
         
         selectPlayer = c.prepareStatement("SELECT * FROM Player WHERE (name LIKE '%?%') ORDER BY name;");
         
-        selectCard = c.prepareStatement("SELECT * FROM Cards WHERE (colour LIKE '%?%') AND (number LIKE '%?%'");
-                
-        deletePlayer = c.prepareStatement("DELETE FROM Player WHERE id =?;");        
+        selectCard = c.prepareStatement("SELECT * FROM Cards WHERE (colour LIKE '%?%') AND (number LIKE '%?%');");
         
-        editPlayer = c.prepareStatement(
-            "UPDATE Kontakte SET name = '?', score = ?, profilePicture = ? WHERE name LIKE = '%?%';");
+        selectCardDarker = c.prepareStatement("SELECT * FROM CardsDark WHERE (colour LIKE '%?%') AND (number LIKE '%?%');");
+                
+        deletePlayer = c.prepareStatement("DELETE FROM Player WHERE (id =?);");        
+        
+        countPlayer = c.prepareStatement("SELECT COUNT (name) FROM Player;");
+        
+        changeName = c.prepareStatement("UPDATE Player SET name = '?' WHERE name LIKE = '%?%';");
+        
+        changeImage = c.prepareStatement("UPDATE Player SET profilePicture = ? WHERE profilePicture LIKE = '%?%';");
+        
     }
     catch(SQLException e) {
       e.printStackTrace();
@@ -46,28 +59,49 @@ public class DatabaseHandler extends database {
   
 
   public void insertPlayer(Player player) {
-
-    System.out.println("insert new Player");
-
+    
     try {
-        insPlayer.setString(1, player.getName());
-        insPlayer.executeUpdate();
+        insertPlayer.setString(1, player.getName());
+        insertPlayer.executeUpdate();
 
     } catch (SQLException e) {    
         e.printStackTrace();
     }
+    System.out.println("insert new Player");
 }
 
   
   public void deletePlayer(Player player) {
-    System.out.println("delete Player");
+   
     try {
         deletePlayer.setInt(1, player.getId());
         deletePlayer.executeUpdate();
     } catch (SQLException e) {       
         e.printStackTrace();
     }
+    System.out.println("delete Player");
   }
+  
+  public Player getPlayer(Player player) {
+    try {
+      selectPlayer.setString(1, player.getName());
+      selectPlayer.executeUpdate();
+      } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return player;
+  } 
+  
+  public boolean checkIfPlayerNew() throws SQLException {
+    int c = 0;
+    ResultSet rs = countPlayer.executeQuery();
+    c = rs.getInt(1);
+    if(c<=0) {
+      return true;
+      }else {
+        return false;
+        }
+    }  
   
   public void selectPlayer(Player player) {
     try {
@@ -78,27 +112,50 @@ public class DatabaseHandler extends database {
     }
   }
   
-  
-  public void editPlayer(Player original, Player neu) {
-    System.out.println("edit existing Player");
+  public void changeName(Player original, Player neu) {    
     try {
-        editPlayer.setString(1, neu.getName());
-      
-        editPlayer.executeUpdate();
-}
-    catch (SQLException e) {       
+        changeName.setString(1, neu.getName());
+        changeName.setString(2, original.getName());      
+        changeName.executeUpdate();
+        
+        } catch (SQLException e) {       
       e.printStackTrace();
     }
   }
-  
-  public Blob getImage(Blob card ) {
 
+  public Image getImage(String colour, String number) {
+    Image img = null;
+    try {    
+      selectCard.setString(1, colour);
+      selectCard.setString(2, number);
+      selectCard.execute();
+      ResultSet rs = selectCard.executeQuery();
+      rs.next();
+      InputStream in = rs.getBinaryStream("image");    
+      img = ImageIO.read(in);
+      } catch (Exception e) {       
+        e.printStackTrace();
+      } 
+    return img;
+      }
+  
+  
+
+  public Image getImageDarker(String colour, String number) {
+    Image img = null;
     try {
-        selectCard.setBlob(1, card.getBinaryStream()); //?
-    }catch (SQLException e) {       
-      e.printStackTrace();
-    }
-    return card;
-  }
-    
+      selectCardDarker.setString(1, colour);
+      selectCardDarker.setString(2, number);
+      selectCardDarker.execute();
+      ResultSet rs = selectCardDarker.executeQuery();
+      rs.next();
+      InputStream in = rs.getBinaryStream("image_Dark");    
+      img = ImageIO.read(in);
+      } catch (Exception e) {       
+        e.printStackTrace();
+      } 
+    return img;
+      }
 }
+  
+
