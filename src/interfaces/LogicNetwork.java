6971f5b39d49abf4c1661006fcff879a34fb5d11
Felix Kleinoeder <fkleinoe@mail.uni-mannheim.de>
2018-04-24 19:58:24 +0200

    #12

--- src/interfaces/LogicNetwork.java
+++ src/interfaces/LogicNetwork.java
@@ -1,113 +1,48 @@
 package interfaces;
 
-import java.util.ArrayList;
+import java.util.List;
+
 import logic.Card;
+import logic.GameSettings;
 import logic.PlayState;
 import logic.Player;
-import logic.Trick;
+import network.server.Server;
 
 //Logic to Network, implemented by Network
-public interface LogicNetwork {
-
-  /**
-   * creates a new skat server
-   */
-  public void createServer();
+public interface LogicNetwork {	
+	//Host game
+	void hostGame(Player player, GameSettings gs);
   
-  /**
-   * sends a played card to all clients
-   */
-  public void sendCard(Card card);
+	//Get Server
+	List<Server> getServer();
   
-  /**
-   * sends a card to a player
-   * @param card
-   * @param player
-   */
-  public void sendCard(Card card, Player player);
+	//Join Lobby, sendet Nachricht zur�ck, ob es geklappt hat (Infos �ber die Lobby werden seperat empfangen)
+	boolean joinLobby(Server server, Player player);
   
-  /**
-   * sends the hand of a player (all his/her cards) to him/her
-   * @param hand
-   */
-  public void sendHand(ArrayList<Card> hand, Player player);
+	//Chat
+	void sendChatMessage(String message);
   
-  /**
-   * sends a bet to the player 
-   * @param bet
-   * @param player
-   */
-  public void askforBet(int bet, Player player);
+	//Settings senden
+	void sendGameSettings(GameSettings gs);
   
-  /**
-   * sends a player (that has to be updated) to all clients 
-   * @param player
-   */
-  public void updatePlayer(Player player);
+	//Spiel start
+	void startGame();
   
-  /**
-   * starts the game on all clients
-   */
-  public void startGame();
+	//Karten dealen
+	void dealCards(Player player, List<Card> cards);
   
-  /**
-   * sends a player to the host/server
-   * @param player
-   */
-  public void joinLobby(Player player);
+	//Your Turn (eigentlich nur am Anfang wichtig)
+	void yourTurn(Player player);
   
-  /**
-   * sends the playstate to all clients
-   * @param ps
-   */
-  public void sendPlayState(PlayState ps);
+	//Bet
+	void bet(int bet);
   
-  /**
-   * ends the play on all clients and shows the winner
-   * @param declarerWon
-   */
-  public void endPlay(boolean declarerWon);
-//  /**
-//   * sends the current trick to all clients
-//   * @param tricks
-//   */
-//  public void updateTrick(Trick tricks);
+	//Play Settings
+	void sendPlayState(PlayState ps);
   
-  /*
-  //Host game
-  void hostGame(Player player, GameSettings gs);
-
-  //Get Server
-  List<Server> getServer();
-
-  //Join Lobby, sendet Nachricht zur�ck, ob es geklappt hat (Infos �ber die Lobby werden seperat empfangen)
-  boolean joinLobby(Server server, Player player);
-
-  //Chat
-  void sendChatMessage(String message);
-
-  //Settings senden
-  void sendGameSettings(GameSettings gs);
-
-  //Spiel start
-  void startGame();
-
-  //Karten dealen
-  void dealCards(Player player, List<Card> cards);
-
-  //Your Turn (eigentlich nur am Anfang wichtig)
-  void yourTurn(Player player);
-
-  //Bet
-  void bet(int bet);
-
-  //Play Settings
-  void sendPlayState(PlayState ps);
-
-  //Card played
-  void sendCardPlayed(Card card);
-
-  //Exit Game
-  void exitGame();
-  */
+	//Card played
+	void sendCardPlayed(Card card);
+  
+	//Exit Game
+	void exitGame();
 }
