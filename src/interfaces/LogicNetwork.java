9c04c5a67fc60b75f4e4e3e405d7ffc935498a0c
Felix Kleinoeder <fkleinoe@mail.uni-mannheim.de>
2018-04-24 13:00:55 +0200

    #12

--- src/interfaces/LogicNetwork.java
+++ src/interfaces/LogicNetwork.java
@@ -67,4 +67,42 @@ public interface LogicNetwork {
 //   * @param tricks
 //   */
 //  public void updateTrick(Trick tricks);
+  
+  /*
+  //Host game
+  void hostGame(Player player, GameSettings gs);
+
+  //Get Server
+  List<Server> getServer();
+
+  //Join Lobby, sendet Nachricht zur�ck, ob es geklappt hat (Infos �ber die Lobby werden seperat empfangen)
+  boolean joinLobby(Server server, Player player);
+
+  //Chat
+  void sendChatMessage(String message);
+
+  //Settings senden
+  void sendGameSettings(GameSettings gs);
+
+  //Spiel start
+  void startGame();
+
+  //Karten dealen
+  void dealCards(Player player, List<Card> cards);
+
+  //Your Turn (eigentlich nur am Anfang wichtig)
+  void yourTurn(Player player);
+
+  //Bet
+  void bet(int bet);
+
+  //Play Settings
+  void sendPlayState(PlayState ps);
+
+  //Card played
+  void sendCardPlayed(Card card);
+
+  //Exit Game
+  void exitGame();
+  */
 }
