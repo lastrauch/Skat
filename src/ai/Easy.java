9cad72a02def6508fce2cba800482bc8a590005d
Duygu Pervane <dpervane@mail.uni-mannheim.de>
2018-04-25 20:13:08 +0200

    #11

--- src/ai/Easy.java
+++ src/ai/Easy.java
@@ -1,14 +1,31 @@
 package ai;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import logic.Card;
+import logic.LogicException;
 
 public class Easy {
   
   
   public static int playCard(AIController controller){
     List<Card> cards = controller.getBot().getHand();
+    List<Card> possibleCards = new ArrayList<Card>();
+    if(controller.getCurrentTrick().size() > 0){
+        for(int i=0; i<cards.size(); i++){
+            try {
+                if(controller.getBot().checkIfCardPossible(cards.get(i), controller.getCurrentTrick().get(0))){
+                    possibleCards.add(cards.get(i));
+                }
+            } catch (LogicException e) {
+                e.printStackTrace();
+            }
+        }
+    }else{
+        possibleCards = cards;
+    }
+    
     
     return 0;
   }
