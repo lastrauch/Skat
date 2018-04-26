dfa82497ef6dd67b09411aef3cf82a098e9657c0
Felix Kleinoeder <fkleinoe@mail.uni-mannheim.de>
2018-04-26 21:04:16 +0200

    #42 Medium

--- src/ai/Medium.java
+++ src/ai/Medium.java
@@ -37,9 +37,25 @@ public class Medium {
   
 	
 	private static SinglePlay playSingle(AIController controller){
+		boolean wantsGrand = false;
+		boolean wantsSuit = false;
+		boolean wantsNull = false;
+		int certGrand = 0;	//Certainty to play Grand, integer in [0;10]
+		int certSuit = 0;	//Certainty to play Suit, integer in [0;10]
+		int certNull = 0;	//Certainty to play Null, integer in [0;10]
+		
+		//Check if AI wants to play Grand
 		
 		
+		//Check if AI wants to play Suit
 		
+		//CHeck if AI wants to play Null
+		
+		//If AI wants to play Grand and Suit, and certainty is the same, check what has the higher game value and play this, set other to not wanted
+		//IF AI wants to play Suit and Null, and certainty is the same, check what has the higher game value and play this
+		//IF AI wants to play Grand and Null, and certainty is the same, always play Grand
+		
+		//If the AI doesn't want to play single, return null
 		return null;
 	}
 	
