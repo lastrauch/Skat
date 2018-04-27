30ad3a02c18360aa5a08ed7467a0601e1acd34ec
Duygu Pervane <dpervane@mail.uni-mannheim.de>
2018-04-27 12:48:44 +0200

    #11 Verbindungsklasse wieder zurückgesetzt

--- src/database/Main.java
+++ src/database/Main.java
@@ -6,8 +6,7 @@ public class Main {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     database = new DatabaseHandler();
-    database.connect();
-    database.prepareStatements();
+    
 
   }
 
