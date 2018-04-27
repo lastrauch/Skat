cf3f2861fe231b6f97e4ee4246d8acaf433636fe
Larissa Strauch <lstrauch@mail.uni-mannheim.de>
2018-04-27 16:57:24 +0200

    #40 kk

--- src/gui/ImplementsLogicGui.java
+++ src/gui/ImplementsLogicGui.java
@@ -1,23 +1,10 @@
 package gui;
 
 import interfaces.LogicGui;
-import logic.GameSettings;
 
 public class ImplementsLogicGui extends GameSettingsController implements LogicGui {
 
   private GuiController guiCon = new GuiController();
-
-
-  @Override
-  public void setGameSettings(GameSettings gs) {
-    // TODO Auto-generated method stub
-  }
-
-  @Override
-  public GuiController getGuiController() {
-    // TODO Auto-generated method stub
-    return guiCon;
-  }
 
   @Override
   public void startInGameScreen() {
