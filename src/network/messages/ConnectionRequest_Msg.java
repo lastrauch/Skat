69965def69869e32dd5cc6e318157f89381bf0cd
Felix Kleinoeder <fkleinoe@mail.uni-mannheim.de>
2018-04-30 15:32:30 +0200

    #12 Debug

--- src/network/messages/ConnectionRequest_Msg.java
+++ src/network/messages/ConnectionRequest_Msg.java
@@ -2,16 +2,16 @@ package network.messages;
 
 import logic.Player;
 
-public class ConnectionRequest_Msg extends Message{
-	private static final long serialVersionUID = 1L;
-	private Player player;
-	
-	public ConnectionRequest_Msg(Player player){
-		super(MessageType.CONNECTION_REQUEST);
-		this.player = player;
-	}
-	
-	public Player getPlayer(){
-		return this.player;
-	}
+public class ConnectionRequest_Msg extends Message {
+  private static final long serialVersionUID = 1L;
+  private Player player;
+
+  public ConnectionRequest_Msg(Player player) {
+    super(MessageType.CONNECTION_REQUEST);
+    this.player = player;
+  }
+
+  public Player getPlayer() {
+    return this.player;
+  }
 }
