b0f2a5052714f56732cafeeab074f9aeefcf392d
Anne Dorothee Wesch <awesch@mail.uni-mannheim.de>
2018-05-01 17:33:52 +0200

    #18 ..

--- src/logic/Position.java
+++ src/logic/Position.java
@@ -1,6 +1,8 @@
 package logic;
 
-public enum Position {
+import java.io.Serializable;
+
+public enum Position implements Serializable{
   FOREHAND(1), MIDDLEHAND(2), REARHAND(3), DEALER(4);
 
   private Position(int i) {}
