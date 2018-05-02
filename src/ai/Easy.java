8523e8f7fd3b89b8763b0c115a953578277a312e
Felix Kleinoeder <fkleinoe@mail.uni-mannheim.de>
2018-05-02 16:19:04 +0200

    #42 askToTakeUpSkat

--- src/ai/Easy.java
+++ src/ai/Easy.java
@@ -5,6 +5,7 @@ import logic.PlayState;
 import java.util.ArrayList;
 import java.util.List;
 import logic.Card;
+import logic.Colour;
 
 public class Easy {
     
@@ -35,9 +36,34 @@ public class Easy {
       skatList.add(controller.getPlayState().getSkat()[i]);
     }
     playState.getDeclarerStack().addCards(skatList);
-    playState.setPlayMode(controller.getSinglePlay().getPlayMode());    
+    playState.setPlayMode(PlayMode.SUIT);
+    if(controller.getSinglePlay().getColour() != null) {
+      playState.setTrump(controller.getSinglePlay().getColour());
+    }else {
+      List<Card> cards = controller.getBot().getHand();
+      int[] hasColour = new int[4];
+      for(int i=0; i<cards.size(); i++) {
+        hasColour[3 - cards.get(i).getColour().ordinal()]++;
+      }
+      int colour = 0;
+      int max = hasColour[0];
+      for(int i=1; i<hasColour.length; i++) {
+        if(hasColour[i] > max) {
+          colour = i;
+          max = hasColour[i];
+        }
+      }
+      Colour c;
+      switch(colour) {
+        case 0: c = Colour.SPADES;
+        case 1: c = Colour.CLUBS;
+        case 2: c = Colour.HEARTS;
+        case 3: c = Colour.DIAMONDS;
+        default: c = Colour.SPADES;
+      }
+      playState.setTrump(c);
+    }
     playState.setSkat(null);
-    playState.setTrump(controller.getSinglePlay().getColour());
     playState.setHandGame(false);
     playState.setSchneiderAnnounced(false);
     playState.setSchwarzAnnounced(false);
