8460f1f3118829a989f689ab754d730bfec384ae
Sandra Beate Angelika Fischer <sandfisc@mail.uni-mannheim.de>
2018-05-04 00:30:34 +0200

    #45 found bugs in play

--- src/jUnit/jLogic/CalculateWinnerTrickTest.java
+++ src/jUnit/jLogic/CalculateWinnerTrickTest.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+
 package jUnit.jLogic;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,10 +23,6 @@ class CalculateWinnerTrickTest {
   static PlayState ps;
   static Trick trick;
 
-  static Card card1;
-  static Card card2;
-  static Card card3;
-
   @BeforeAll
   static void setUpBeforeClass() {
     ps = new PlayState(new Player[] {new Player("P1"), new Player("P2"), new Player("P3")});
@@ -44,13 +38,9 @@ class CalculateWinnerTrickTest {
     ps.setPlayMode(PlayMode.SUIT);
     ps.setTrump(Colour.HEARTS);
 
-    card1 = new Card(Colour.HEARTS, Number.TEN);
-    card2 = new Card(Colour.HEARTS, Number.JACK);
-    card3 = new Card(Colour.CLUBS, Number.ASS);
-
-    trick.addCard(card1);
-    trick.addCard(card2);
-    trick.addCard(card3);
+    trick.addCard(new Card(Colour.HEARTS, Number.TEN));
+    trick.addCard(new Card(Colour.HEARTS, Number.JACK));
+    trick.addCard(new Card(Colour.CLUBS, Number.ASS));
 
     assertEquals(trick.calculateWinner(ps), ps.getGroup()[1]);
     assertNotEquals(trick.calculateWinner(ps), ps.getGroup()[0]);
@@ -60,14 +50,10 @@ class CalculateWinnerTrickTest {
   @Test
   void testGrand() throws LogicException {
     ps.setPlayMode(PlayMode.GRAND);
-
-    card1 = new Card(Colour.HEARTS, Number.TEN);
-    card2 = new Card(Colour.HEARTS, Number.JACK);
-    card3 = new Card(Colour.CLUBS, Number.JACK);
-
-    trick.addCard(card1);
-    trick.addCard(card2);
-    trick.addCard(card3);
+    
+    trick.addCard(new Card(Colour.HEARTS, Number.TEN));
+    trick.addCard(new Card(Colour.HEARTS, Number.JACK));
+    trick.addCard(new Card(Colour.CLUBS, Number.JACK));
 
     assertEquals(trick.calculateWinner(ps), ps.getGroup()[2]);
     assertNotEquals(trick.calculateWinner(ps), ps.getGroup()[0]);
@@ -78,13 +64,9 @@ class CalculateWinnerTrickTest {
   void testNull() throws LogicException {
     ps.setPlayMode(PlayMode.NULL);
 
-    card1 = new Card(Colour.HEARTS, Number.TEN);
-    card2 = new Card(Colour.HEARTS, Number.ASS);
-    card3 = new Card(Colour.CLUBS, Number.JACK);
-
-    trick.addCard(card1);
-    trick.addCard(card2);
-    trick.addCard(card3);
+    trick.addCard(new Card(Colour.HEARTS, Number.TEN));
+    trick.addCard(new Card(Colour.HEARTS, Number.ASS));
+    trick.addCard(new Card(Colour.CLUBS, Number.JACK));
 
     assertEquals(trick.calculateWinner(ps), ps.getGroup()[1]);
     assertNotEquals(trick.calculateWinner(ps), ps.getGroup()[0]);
---
+++ src/jUnit/jLogic/PlayTest.java
@@ -0,0 +1,181 @@
+/**
+ * 
+ */
+package jUnit.jLogic;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import logic.Card;
+import logic.Colour;
+import logic.CountRule;
+import logic.GameSettings;
+import logic.LogicException;
+import logic.Number;
+import logic.Play;
+import logic.PlayState;
+import logic.Player;
+import logic.Tools;
+
+/**
+ * @author sandfisc
+ *
+ */
+class PlayTest {
+
+  static PlayState ps;
+  static GameSettings gs;
+
+  static Player[] expectedWinner;
+  static Player[] expectedLooser;
+
+  static List<Card> winnerCards;
+  static List<Card> looserCards;
+
+  static Player p1;
+  static Player p2;
+  static Player p3;
+
+  @BeforeAll
+  static void setUpBeforeClass() {
+    
+    winnerCards = new ArrayList<Card>();
+    winnerCards.add(new Card(Colour.SPADES, Number.ASS));
+    winnerCards.add(new Card(Colour.SPADES, Number.TEN));
+    winnerCards.add(new Card(Colour.SPADES, Number.KING));
+
+    looserCards = new ArrayList<Card>();
+    looserCards.add(new Card(Colour.SPADES, Number.QUEEN));
+    looserCards.add(new Card(Colour.SPADES, Number.JACK));
+    looserCards.add(new Card(Colour.SPADES, Number.NINE));
+
+    expectedWinner = new Player[2];
+    expectedLooser = new Player[2];
+
+    gs = new GameSettings();
+  }
+
+  @BeforeEach
+  void setUp() {
+    p1 = new Player("P1");
+    p1.setBet(18);
+    p1.setDeclarer(true);
+
+    p2 = new Player("P2");
+    p2.setBet(-1);
+    p2.setDeclarer(false);
+
+    p3 = new Player("P3");
+    p3.setBet(-1);
+    p3.setDeclarer(false);
+
+    ps = new PlayState(new Player[] {p1, p2, p3});
+    ps.setPlayValue(44);
+  }
+
+  @Test 
+  void testOverBid(){
+    p1.setBet(60);
+    assertTrue(Play.checkOverBid(ps));
+  }
+  
+  /* Winner (Standard) Opponents(No OverBid, No Schwarz, No Schneider) */
+  @Test
+  void testWinnerOpponentsNormal() {
+    ps.getDeclarerStack().addCards(looserCards);
+    ps.getOpponentsStack().addCards(winnerCards);
+
+    expectedWinner[0] = p2;
+    expectedWinner[1] = p3;
+    expectedLooser[0] = p1;
+    expectedLooser[1] = null;
+
+    this.testWinner();
+  }
+
+  /* Winner (Standard) Declarer(No OverBid, No Schwarz, No Schneider) */
+  @Test
+  void testWinnerDeclarerNormal() {
+    ps.getDeclarerStack().addCards(winnerCards);
+    ps.getOpponentsStack().addCards(looserCards);
+
+    expectedWinner[0] = p1;
+    expectedWinner[1] = null;
+    expectedLooser[0] = p2;
+    expectedLooser[1] = p3;
+
+    this.testWinner();
+  }
+
+  void testWinner() {
+    assertFalse(Play.checkOverBid(ps));
+    assertEquals(Play.calculateWinner(ps)[0], expectedWinner[0]);
+    assertEquals(Play.calculateWinner(ps)[1], expectedWinner[1]);
+    assertNotEquals(Play.calculateWinner(ps)[0], expectedLooser[0]);
+    assertNotEquals(Play.calculateWinner(ps)[1], expectedLooser[1]);
+  }
+
+  /* Calculate Points Normal (Declarer won) */
+  @Test 
+  void testCalculatePointsNormal1() throws LogicException {
+    gs.setCountRule(CountRule.NORMAL);
+    Play.calculatePoints(ps, gs, true);
+    assertEquals(p1.getGamePoints(), ps.getPlayValue());
+    assertEquals(p2.getGamePoints(), 0);
+    assertEquals(p3.getGamePoints(), 0);
+  }
+  
+  /* Calculate Points Normal (Opponents won) */
+  @Test 
+  void testCalculatePointsNormal2() throws LogicException {
+    gs.setCountRule(CountRule.NORMAL);
+    Play.calculatePoints(ps, gs, false);
+    assertEquals(p1.getGamePoints(), ps.getPlayValue() * (-2));
+    assertEquals(p2.getGamePoints(), 0);
+    assertEquals(p3.getGamePoints(), 0);
+  }
+  
+  /* Calculate Points Bierlachs (Declarer won) */
+  @Test 
+  void testCalculatePointsBierlachs1() throws LogicException {
+    gs.setCountRule(CountRule.BIERLACHS);
+    Play.calculatePoints(ps, gs, true);
+    assertEquals(p1.getGamePoints(), 0);
+    assertEquals(p2.getGamePoints(), ps.getPlayValue() * (-1));
+    assertEquals(p3.getGamePoints(), ps.getPlayValue() * (-1));
+  }
+
+  /* Calculate Points Bierlachs (Opponents won) */
+  @Test
+  void testCalculatePointsBierlachs2() throws LogicException {
+    gs.setCountRule(CountRule.BIERLACHS);
+    Play.calculatePoints(ps, gs, false);
+    assertEquals(p1.getGamePoints(), ps.getPlayValue() * (-2));
+    assertEquals(p2.getGamePoints(), 0);
+    assertEquals(p3.getGamePoints(), 0);
+  }
+  
+  /* Calculate Points Seegerfabian (Opponents won) */
+  @Test
+  void testCalculatePointsSeeger1() throws LogicException {
+    gs.setCountRule(CountRule.SEEGERFABIAN);
+    Play.calculatePoints(ps, gs, true);
+    assertEquals(p1.getGamePoints(), ps.getPlayValue() + 50);
+    assertEquals(p2.getGamePoints(), 0);
+    assertEquals(p3.getGamePoints(), 0);
+  }
+  
+  /* Calculate Points Seegerfabian (Opponents won) */
+  @Test
+  void testCalculatePointsSeeger2() throws LogicException {
+    gs.setCountRule(CountRule.SEEGERFABIAN);
+    Play.calculatePoints(ps, gs, false);
+    assertEquals(p1.getGamePoints(), (-1) * (ps.getPlayValue() + 50));
+    assertEquals(p2.getGamePoints(), 0);
+    assertEquals(p3.getGamePoints(), 0);
+  }
+}
