017ec5b2b6a42574fafef79438a2e3690b571fea
Felix Kleinoeder <fkleinoe@mail.uni-mannheim.de>
2018-05-04 02:15:40 +0200

    #42 Commend

--- src/ai/Bot.java
+++ src/ai/Bot.java
@@ -3,18 +3,47 @@ package ai;
 import logic.Player;
 
 public class Bot extends Player {
+	
+	// This is a class that represents a bot. It has the same and more attributes
+	// and functions like Player, which it extends.
+	
 	private static final long serialVersionUID = 1L;
 	private BotDifficulty difficulty;
 
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Constructor
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/**
+	 * Constucts a bot with given difficulty.
+	 * 
+	 * @author fkleinoe
+	 * @param name
+	 * @param difficulty
+	 */
 	public Bot(String name, BotDifficulty difficulty) {
 		super(name, true);
 		this.difficulty = difficulty;
 	}
 
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Setter- and Getter-Methods
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/**
+	 * Set difficulty.
+	 * 
+	 * @author fkleinoe
+	 * @param difficulty
+	 */
 	public void setDifficulty(BotDifficulty difficulty) {
 		this.difficulty = difficulty;
 	}
 
+	/**
+	 * Get difficutly.
+	 * 
+	 * @author fkleinoe
+	 * @return BotDifficulty
+	 */
 	public BotDifficulty getDifficulty() {
 		return this.difficulty;
 	}
--- src/ai/BotDifficulty.java
+++ src/ai/BotDifficulty.java
@@ -1,5 +1,6 @@
 package ai;
 
+//This is an enum, that represents the difficulty of a bot.
 public enum BotDifficulty {
   EASY,
   MEDIUM,
