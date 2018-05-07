ef96334bdbd5abc273d8cba498f4a0e099f095c8
Anne Dorothee Wesch <awesch@mail.uni-mannheim.de>
2018-05-07 16:57:02 +0200

    #18 ..

--- src/logic/ClientLogic.java
+++ src/logic/ClientLogic.java
@@ -568,7 +568,7 @@ public class ClientLogic implements NetworkLogic, AILogic {
   public void playCard(Card firstCard) {
     this.inGameController.itsYourTurn();
     this.waitFor(1000);
-    int indexNewCard = this.inGameController.askToPlayCard();
+    int indexNewCard = this.inGameController.askToPlayCard(this.gameSettings.getTimeLimit());
     // because we had some to high results from askToPlayCard
     if (indexNewCard >= this.player.getHand().size()) {
       this.playCard(firstCard);
@@ -746,7 +746,7 @@ public class ClientLogic implements NetworkLogic, AILogic {
 
         // show winner of play
         // this.inGameController.showWinnerPlay(playWinner[0], playWinner[1]);
-        this.inGameController.showPoints(this.group);
+        this.inGameController.showScore(this.group);
         if (playWinner[0].getName().equals(this.player.getName())
             || playWinner[1].getName().equals(this.player.getName())) {
           System.out.println(this.player.getName() + ": I won the play!!");
@@ -765,7 +765,7 @@ public class ClientLogic implements NetworkLogic, AILogic {
 
           this.waitFor(3000);
           // show winner of game
-          this.inGameController.showPoints(this.group);
+          this.inGameController.showScore(this.group);
           if (gameWinner[0].getName().equals(this.player.getName())) {
             System.out.println(this.player.getName() + ": I won the game!!");
           }
