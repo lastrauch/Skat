f892b2a6ce19ea8e39f77bc6b604ccdbf0e954b8
Felix Kleinoeder <fkleinoe@mail.uni-mannheim.de>
2018-05-07 18:53:39 +0200

    #42 design

--- src/ai/General.java
+++ src/ai/General.java
@@ -10,156 +10,156 @@ import logic.PlayMode;
 
 public class General {
 
-    // This is a static class to implement methods which are used through different
-    // Bot-difficulties.
-    // Available Methods are:
+	// This is a static class to implement methods which are used through different
+	// Bot-difficulties.
+	// Available Methods are:
 
-    // playRandomCard(AIController) : int
-    // Returns the index of a playable card on the hand of the bot.
+	// playRandomCard(AIController) : int
+	// Returns the index of a playable card on the hand of the bot.
 
-    // getHighesPossibleBet(AIController) : int
-    // Returns the highest possible bet, with the current hand of the bot
+	// getHighesPossibleBet(AIController) : int
+	// Returns the highest possible bet, with the current hand of the bot
 
-    // getHighestPossibleBet(AIController, PlayMode) : int
-    // Returns the highest possible bet, playing PlayMode
+	// getHighestPossibleBet(AIController, PlayMode) : int
+	// Returns the highest possible bet, playing PlayMode
 
-    // getGameLevel(AIController) : int
-    // Returns the game level, the bot is playing
+	// getGameLevel(AIController) : int
+	// Returns the game level, the bot is playing
 
-    // with initializeProbabilities(List<Card>) : double[][]
-    // Returns the initialization of card probabilities, first column is bot
+	// with initializeProbabilities(List<Card>) : double[][]
+	// Returns the initialization of card probabilities, first column is bot
 
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Internal methods
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /**
-     * Returns the index of a playable card on the hand of the bot.
-     * 
-     * @author fkleinoe
-     * @param controller
-     * @return int
-     */
-    public static int playRandomCard(AiController controller) {
-        List<Card> cards = controller.getBot().getHand();
-        List<Card> possibleCards = new ArrayList<Card>();
-        if (controller.getCurrentTrick().size() > 0) {
-            for (int i = 0; i < cards.size(); i++) {
-                try {
-                    if (ClientLogic.checkIfCardPossible(cards.get(i), controller.getCurrentTrick().get(0),
-                            controller.getPlayState(), controller.getBot())) {
-                        possibleCards.add(cards.get(i));
-                    }
-                } catch (LogicException e) {
-                    e.printStackTrace();
-                }
-            }
-        } else {
-            possibleCards = cards;
-        }
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Internal methods
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/**
+	 * Returns the index of a playable card on the hand of the bot.
+	 * 
+	 * @author fkleinoe
+	 * @param controller
+	 * @return int
+	 */
+	public static int playRandomCard(AiController controller) {
+		List<Card> cards = controller.getBot().getHand();
+		List<Card> possibleCards = new ArrayList<Card>();
+		if (controller.getCurrentTrick().size() > 0) {
+			for (int i = 0; i < cards.size(); i++) {
+				try {
+					if (ClientLogic.checkIfCardPossible(cards.get(i), controller.getCurrentTrick().get(0),
+							controller.getPlayState(), controller.getBot())) {
+						possibleCards.add(cards.get(i));
+					}
+				} catch (LogicException e) {
+					e.printStackTrace();
+				}
+			}
+		} else {
+			possibleCards = cards;
+		}
 
-        int rnd = (int) (Math.random() * possibleCards.size());
-        Card playCard = possibleCards.get(rnd);
-        int index = cards.indexOf(playCard);
+		int rnd = (int) (Math.random() * possibleCards.size());
+		Card playCard = possibleCards.get(rnd);
+		int index = cards.indexOf(playCard);
 
-        return index;
-    }
+		return index;
+	}
 
-    /**
-     * Returns the highest possible bet, with the current hand of the bot
-     * 
-     * @author fkleinoe
-     * @param controller
-     * @return int
-     */
-    public static int getHighestPossibleBet(AiController controller) {
-        return getHighestPossibleBet(controller, PlayMode.GRAND);
-    }
+	/**
+	 * Returns the highest possible bet, with the current hand of the bot
+	 * 
+	 * @author fkleinoe
+	 * @param controller
+	 * @return int
+	 */
+	public static int getHighestPossibleBet(AiController controller) {
+		return getHighestPossibleBet(controller, PlayMode.GRAND);
+	}
 
-    /**
-     * Returns the highest possible bet, playing PlayMode
-     * 
-     * @author fkleinoe
-     * @param controller
-     * @param playMode
-     * @return
-     */
-    public static int getHighestPossibleBet(AiController controller, PlayMode playMode) {
-        if (playMode == PlayMode.NULL) {
-            return 23;
-        } else {
-            int gameLevel = General.getGameLevel(controller);
-            if (playMode == PlayMode.GRAND) {
-                return 24 * gameLevel;
-            } else {
-                return 12 * gameLevel;
-            }
-        }
-    }
+	/**
+	 * Returns the highest possible bet, playing PlayMode
+	 * 
+	 * @author fkleinoe
+	 * @param controller
+	 * @param playMode
+	 * @return
+	 */
+	public static int getHighestPossibleBet(AiController controller, PlayMode playMode) {
+		if (playMode == PlayMode.NULL) {
+			return 23;
+		} else {
+			int gameLevel = General.getGameLevel(controller);
+			if (playMode == PlayMode.GRAND) {
+				return 24 * gameLevel;
+			} else {
+				return 12 * gameLevel;
+			}
+		}
+	}
 
-    /**
-     * Returns the game level, the bot is playing
-     * 
-     * @author fkleinoe
-     * @param controller
-     * @return int
-     */
-    public static int getGameLevel(AiController controller) {
-        List<Card> cards = controller.getBot().getHand();
-        // Determine the Jacks
-        boolean[] jacks = new boolean[4];
-        for (int i = 0; i < cards.size(); i++) {
-            if (cards.get(i).getNumber() == Number.JACK) {
-                switch (cards.get(i).getColour()) {
-                case CLUBS:
-                    jacks[0] = true;
-                    break;
-                case SPADES:
-                    jacks[1] = true;
-                    break;
-                case HEARTS:
-                    jacks[2] = true;
-                    break;
-                case DIAMONDS:
-                    jacks[3] = true;
-                    break;
-                }
-            }
-        }
+	/**
+	 * Returns the game level, the bot is playing
+	 * 
+	 * @author fkleinoe
+	 * @param controller
+	 * @return int
+	 */
+	public static int getGameLevel(AiController controller) {
+		List<Card> cards = controller.getBot().getHand();
+		// Determine the Jacks
+		boolean[] jacks = new boolean[4];
+		for (int i = 0; i < cards.size(); i++) {
+			if (cards.get(i).getNumber() == Number.JACK) {
+				switch (cards.get(i).getColour()) {
+				case CLUBS:
+					jacks[0] = true;
+					break;
+				case SPADES:
+					jacks[1] = true;
+					break;
+				case HEARTS:
+					jacks[2] = true;
+					break;
+				case DIAMONDS:
+					jacks[3] = true;
+					break;
+				}
+			}
+		}
 
-        // Determine the gameLevel
-        int gameLevel = 1;
-        boolean with = jacks[0];
-        for (int i = 1; i < 4; i++) {
-            if (jacks[0] == with) {
-                gameLevel++;
-            }
-        }
-        return gameLevel;
-    }
+		// Determine the gameLevel
+		int gameLevel = 1;
+		boolean with = jacks[0];
+		for (int i = 1; i < 4; i++) {
+			if (jacks[0] == with) {
+				gameLevel++;
+			}
+		}
+		return gameLevel;
+	}
 
-    /**
-     * Returns the initialization of card probabilities, first column is bot
-     * 
-     * @author fkleinoe
-     * @param hand
-     * @return double[][]
-     */
-    public static double[][] initializeProbabilities(List<Card> hand) {
-        double prob[][] = new double[32][3];
-        for (int i = 0; i < prob.length; i++) {
-            for (int j = 1; j < prob[0].length; j++) {
-                prob[i][j] = 0.5;
-            }
-        }
-        int colour;
-        int number;
-        for (int i = 0; i < hand.size(); i++) {
-            colour = 3 - hand.get(i).getColour().ordinal();
-            number = 7 - hand.get(i).getNumber().ordinal();
-            prob[colour * 8 + number][0] = 1;
-            prob[colour * 8 + number][1] = 0;
-            prob[colour * 8 + number][2] = 0;
-        }
+	/**
+	 * Returns the initialization of card probabilities, first column is bot
+	 * 
+	 * @author fkleinoe
+	 * @param hand
+	 * @return double[][]
+	 */
+	public static double[][] initializeProbabilities(List<Card> hand) {
+		double prob[][] = new double[32][3];
+		for (int i = 0; i < prob.length; i++) {
+			for (int j = 1; j < prob[0].length; j++) {
+				prob[i][j] = 0.5;
+			}
+		}
+		int colour;
+		int number;
+		for (int i = 0; i < hand.size(); i++) {
+			colour = 3 - hand.get(i).getColour().ordinal();
+			number = 7 - hand.get(i).getNumber().ordinal();
+			prob[colour * 8 + number][0] = 1;
+			prob[colour * 8 + number][1] = 0;
+			prob[colour * 8 + number][2] = 0;
+		}
 		return prob;
 	}
 
