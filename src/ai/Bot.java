ff4640c8ee1b3841f9d3572071806748107a480e
Felix Kleinoeder <fkleinoe@mail.uni-mannheim.de>
2018-05-09 11:09:01 +0200

    #42 style

--- src/ai/Bot.java
+++ src/ai/Bot.java
@@ -3,49 +3,49 @@ package ai;
 import logic.Player;
 
 public class Bot extends Player {
-	
-	// This is a class that represents a bot. It has the same and more attributes
-	// and functions like Player, which it extends.
 
-	private static final long serialVersionUID = 1L;
-	private BotDifficulty difficulty;
+  // This is a class that represents a bot. It has the same and more attributes
+  // and functions like Player, which it extends.
 
-	//////////////////////////////////////////////////////////////////////////////////////////////////
-	// Constructor
-	//////////////////////////////////////////////////////////////////////////////////////////////////
-	/**
-	 * Constucts a bot with given difficulty.
-	 * 
-	 * @author fkleinoe
-	 * @param name
-	 * @param difficulty
-	 */
-	public Bot(String name, BotDifficulty difficulty) {
-		super(name, true);
-		this.difficulty = difficulty;
-	}
+  private static final long serialVersionUID = 1L;
+  private BotDifficulty difficulty;
 
-	//////////////////////////////////////////////////////////////////////////////////////////////////
-	// Setter- and Getter-Methods
-	//////////////////////////////////////////////////////////////////////////////////////////////////
-	/**
-	 * Set difficulty.
-	 * 
-	 * @author fkleinoe
-	 * @param difficulty
-	 */
-	public void setDifficulty(BotDifficulty difficulty) {
-		this.difficulty = difficulty;
-	}
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  // Constructor
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  /**
+   * Constucts a bot with given difficulty.
+   * 
+   * @author fkleinoe
+   * @param name
+   * @param difficulty
+   */
+  public Bot(String name, BotDifficulty difficulty) {
+    super(name, true);
+    this.difficulty = difficulty;
+  }
 
-	/**
-	 * Get difficutly.
-	 * 
-	 * @author fkleinoe
-	 * @return BotDifficulty
-	 */
-	public BotDifficulty getDifficulty() {
-		return this.difficulty;
-	}
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  // Setter- and Getter-Methods
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+  /**
+   * Set difficulty.
+   * 
+   * @author fkleinoe
+   * @param difficulty
+   */
+  public void setDifficulty(BotDifficulty difficulty) {
+    this.difficulty = difficulty;
+  }
+
+  /**
+   * Get difficutly.
+   * 
+   * @author fkleinoe
+   * @return BotDifficulty
+   */
+  public BotDifficulty getDifficulty() {
+    return this.difficulty;
+  }
 
 }
