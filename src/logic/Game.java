4bae75ae399a039b7e3912d0643ab1e14a30f0b4
Anne Dorothee Wesch <awesch@mail.uni-mannheim.de>
2018-05-09 12:53:56 +0200

    #18 ..

--- src/logic/Game.java
+++ src/logic/Game.java
@@ -1,19 +1,49 @@
 package logic;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Game {
 
   /**
    * calculates the winner of the whole game.
    * 
    * @author sandfisc
+   * @author awesch
    */
-  public static Player calculateWinner(PlayState ps) {
-    Player winner = ps.getGroup()[0];
-    for (int i = 1; i < ps.getGroup().length; i++) {
-      if (ps.getGroup()[i].getGameScore() > winner.getGameScore()) {
-        winner = ps.getGroup()[i];
+  public static List<Player> calculateWinner(PlayState ps, List<Player> group) {
+    List<Player> winner = new ArrayList<Player>();
+    int highestScore = 0;
+
+    // go through the group
+    for (Player p : group) {
+      int score = 0;
+
+      // go through the playScore
+      for (int i : p.getPlayScore()) {
+
+        // if score = -1 the player was dealer in this play
+        if (i != -1) {
+          score += i;
+        }
+      }
+
+      // save score in GameScore
+      p.setGameScore(score);
+
+      // set highest score
+      if (p.getGameScore() > highestScore) {
+        highestScore = p.getGameScore();
       }
     }
+
+    // go through the group again and check if highest score
+    for (Player p : group) {
+      if (p.getGameScore() == highestScore) {
+        winner.add(p);
+      }
+    }
+
     return winner;
   }
 }
