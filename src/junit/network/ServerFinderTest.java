3097eff025b36d5215ec9cc7d8aae1f10d52c5cb
Sandra Beate Angelika Fischer <sandfisc@mail.uni-mannheim.de>
2018-05-09 19:25:54 +0200

    #18 ......

--- src/junit/network/ServerFinderTest.java
+++ src/junit/network/ServerFinderTest.java
@@ -1,43 +1,6 @@
 package junit.network;
 
-<<<<<<< HEAD
-import static org.junit.Assert.fail;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-public class ServerFinderTest {
-
-  @BeforeClass
-  public static void setUpBeforeClass() throws Exception {}
-
-  @AfterClass
-  public static void tearDownAfterClass() throws Exception {}
-
-  @Before
-  public void setUp() throws Exception {}
-
-  @After
-  public void tearDown() throws Exception {}
-
-  @Test
-  public void testServerFinder() {
-    fail("Not yet implemented");
-  }
-
-  @Test
-  public void testRefresh() {
-    fail("Not yet implemented");
-  }
-
-  @Test
-  public void testGetServers() {
-    fail("Not yet implemented");
-  }
-=======
 import static org.junit.Assert.*;
 
 import org.junit.After;
@@ -78,6 +41,5 @@ public class ServerFinderTest {
 	public void testGetServers() {
 		fail("Not yet implemented");
 	}
->>>>>>> refs/heads/database
 
 }
