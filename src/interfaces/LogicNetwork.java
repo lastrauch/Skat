cad3d133aad9f99f8617ff4a6ef46d42d437782e
Anne Dorothee Wesch <awesch@mail.uni-mannheim.de>
2018-05-10 06:29:21 +0200

    #18 ..

--- src/interfaces/LogicNetwork.java
+++ src/interfaces/LogicNetwork.java
@@ -9,13 +9,13 @@ import network.server.Server;
 
 // Logic to Network, implemented by Network
 public interface LogicNetwork {
-  
+
   /**
    * should open a new lobby and return the clients server.
    * 
-   * @param player
-   * @param gs
-   * @param comment
+   * @param player who hosts the game
+   * @param gs gameSettings
+   * @param comment to the others
    * @return the clients own server
    */
   public Server hostGame(Player player, GameSettings gs, String comment);
@@ -30,25 +30,25 @@ public interface LogicNetwork {
   /**
    * to join a lobby.
    * 
-   * @param server
-   * @param player
-   * @return if it worked
+   * @param server the player wants to join
+   * @param player who wants to join
+   * @return true if it worked
    */
   public boolean joinLobby(Server server, Player player);
 
   /**
    * sends a chat message to the other clients.
    * 
-   * @param message
+   * @param message to the others
    */
   public void sendChatMessage(Player player, String message);
-  
+
   /**
    * sends out, if a player announced kontra.
    * 
    */
   public void sendKontra();
-  
+
   /**
    * sends out, that a player wants to rekontra.
    * 
@@ -58,7 +58,7 @@ public interface LogicNetwork {
   /**
    * sends out the game settings to other clients.
    * 
-   * @param gs
+   * @param gs gameSettings
    */
   public void sendGameSettings(GameSettings gs);
 
@@ -71,32 +71,32 @@ public interface LogicNetwork {
   /**
    * gives a player his hand cards.
    * 
-   * @param player
-   * @param cards
-   * @param ps
+   * @param player to whom the cards are dealt
+   * @param cards for the hand
+   * @param ps playState
    */
   public void dealCards(Player player, List<Card> cards, PlayState ps);
-  
+
   /**
    * sends out the bet of a player.
    * 
-   * @param bet
-   * @param player
+   * @param bet of the player
+   * @param player who bet
    */
   public void bet(int bet, Player player);
 
   /**
    * sends out the play state to all other clients.
    * 
-   * @param ps
+   * @param ps playState
    */
   public void sendPlayState(PlayState ps);
 
   /**
-   * lets the clients know, tha a card was played.
+   * lets the clients know, that a card was played.
    * 
-   * @param card
-   * @param player
+   * @param card player
+   * @param player who played the card
    */
   public void sendCardPlayed(Card card, Player player);
 
