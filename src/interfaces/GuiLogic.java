aa234031f1e45340d6b232daaa2cb624baa48e0e
Sandra Beate Angelika Fischer <sandfisc@mail.uni-mannheim.de>
2018-05-10 06:28:02 +0200

    #18 ....

--- src/interfaces/GuiLogic.java
+++ src/interfaces/GuiLogic.java
@@ -31,59 +31,63 @@ public interface GuiLogic {
   
   /**
    * Should delete an already existing bot if difficulty has changed.
-   * @param botname
+   * @param botname of the bot that should be deleted.
    */
   public void deleteBot(String botname); 
   
   /**
-   * Should create a new bot with the set difficulty
-   * @param botname
-   * @param difficulty
+   * Should create a new bot with the set difficulty.
+   * @param botname of the bot that shoult be setted.
+   * @param difficulty of the (new) bot.
    */
+  
   public void setBot(String botname, BotDifficulty difficulty);
   
   /**
-   * send the just tipped chat message
-   * @param message
+   * send the just tipped chat message.
+   * @param message the over players get.
    */
   public void sendChatText(String message);
   
   /**
-   * @param hostName
+   * The client joins a lobby.
+   * @param ip of the server, the client wants to join.
    */
   public void joinGame(String ip); 
   
   /**
-   * @param comment
+   * A new skat server is started.
+   * @param comment the host uses to describe his/her server.
    */
   public void hostGame(String comment, GameSettings gs);
   
   /**
-   * supposed to  skat the cards from jack to trump to lowest value
-   * @return
+   * supposed to sort the cards from jack to trump to lowest value.
+   * @param ps PlayState
+   * @return the sorted array of cards
    */
   public ArrayList<Card> sortHand(PlayState ps, List<Card> cardlist); 
   
   /**
-   * starts the game afte SinglePlayer Lobby
-   * @param gs
+   * starts the game after SinglePlayer Lobby.
+   * @param gs GameSettings the Host sends to all clients.
    */
   public void startGame(GameSettings gs);
   
   /**
-   * should return how many players are in one server, what's the server's name and his message
-   * @return
+   * should return how many players are in one server, what's the server's name and his message.
+   * @return List of server
    */
   public ArrayList<Server> lobbyInformation();
   
   /**
-   * returns the current player
-   * @return
+   * returns the current player.
+   * @return player
    */
   public Player getPlayer();
   
   /**
-   * announces kontra after beeing clicked
+   * announces kontra after beeing clicked.
    * @author lstrauch
    */
   public void announceContra();
