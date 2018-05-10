5756ccf17c37c0a2969ae28bff13b5f9e8126a03
Anne Dorothee Wesch <awesch@mail.uni-mannheim.de>
2018-05-10 02:46:02 +0200

    #11 .

--- src/database/CardImage.java
+++ src/database/CardImage.java
@@ -11,7 +11,11 @@ public class CardImage {
   private Image img;
 
   public CardImage(String colour, String number, String dark, Image image) {
-    
+    Colour col = this.setColour(colour);
+    Number nr = this.setNumber(number);
+    this.setCard(new Card(col, nr));
+    this.setDark(this.setDark(dark));
+    this.setImg(image);
   }
 
   public Colour setColour(String col) {
@@ -34,5 +38,70 @@ public class CardImage {
     }
     return c;
   }
+
+  public Number setNumber(String nr) {
+    Number number = Number.SEVEN;
+    switch (nr) {
+      case "seven":
+        number = Number.SEVEN;
+        break;
+      case "eight":
+        number = Number.EIGHT;
+        break;
+      case "nine":
+        number = Number.NINE;
+        break;
+      case "jack":
+        number = Number.JACK;
+        break;
+      case "queen":
+        number = Number.QUEEN;
+        break;
+      case "king":
+        number = Number.KING;
+        break;
+      case "ten":
+        number = Number.TEN;
+        break;
+      case "ass":
+        number = Number.ASS;
+        break;
+      default:
+        break;
+    }
+    return number;
+  }
+
+  public boolean setDark(String dark) {
+    if (dark.equals("dark")) {
+      return true;
+    }
+    return false;
+  }
+
+  public Card getCard() {
+    return card;
+  }
+
+  public void setCard(Card card) {
+    this.card = card;
+  }
+
+  public boolean isDark() {
+    return dark;
+  }
+
+  public void setDark(boolean dark) {
+    this.dark = dark;
+  }
+
+  public Image getImg() {
+    return img;
+  }
+
+  public void setImg(Image img) {
+    this.img = img;
+  }
+  
 
 }
