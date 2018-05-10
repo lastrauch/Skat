19d6643c18d6b4c5fc1b775a5ee3b74647b4f0e9
Felix Kleinoeder <fkleinoe@mail.uni-mannheim.de>
2018-05-10 06:16:50 +0200

    #42 junit

--- src/junit/ai/AiControllerTest.java
+++ src/junit/ai/AiControllerTest.java
@@ -1,104 +1,198 @@
 package junit.ai;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
+import ai.AiController;
+import ai.Bot;
+import ai.BotDifficulty;
+import ai.SinglePlay;
+import java.util.ArrayList;
+import java.util.List;
+import logic.Card;
+import logic.ClientLogic;
+import logic.Colour;
+import logic.GameSettings;
+import logic.Number;
+import logic.PlayMode;
+import logic.PlayState;
+import logic.Player;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Tests methods of the AiController class.
+ * 
+ * @author fkleinoe
+ */
 class AiControllerTest {
+  static AiController controller;
+  static ClientLogic logic;
+  static GameSettings gameSettings;
+  static Bot bot;
+  PlayState playState;
+  List<Player> player;
+  List<Player> opponents;
+  Player partner;
+  int[] bets;
+  int maxBet;
+  SinglePlay singlePlay;
+  Card[][] playedCards;
+  double[][] cardProbability;
+  boolean[][] hasColour;
+  boolean[] hasTrump;
+  int existingTrumps;
+  List<Card> currentTrick;
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
+  /**
+   * Set up before class.
+   * 
+   * @author fkleinoe
+   * @throws Exception e
+   */
+  @BeforeAll
+  static void setUpBeforeClass() throws Exception {
+    logic = new ClientLogic(new Player("Felix"));
+    gameSettings = new GameSettings();
+    controller = new AiController(logic, "Name", BotDifficulty.EASY, gameSettings);
+    bot = controller.getBot();
+  }
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
+  /**
+   * Set up before each method.
+   * 
+   * @author fkleinoe
+   * @throws Exception e
+   */
+  @BeforeEach
+  void setUp() throws Exception {
+    player = new ArrayList<Player>();
+    player.add(bot);
+    controller.setPlayer(player);
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+    opponents = new ArrayList<Player>();
+    bets = new int[3];
+    playedCards = new Card[1][3];
+    cardProbability = new double[32][3];
+    hasColour = new boolean[4][3];
+    hasTrump = new boolean[3];
+    currentTrick = new ArrayList<Card>();
+  }
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
 
-	@Test
-	void testAiController() {
-		fail("Not yet implemented");
-	}
+  /**
+   * Check if player are added correctly.
+   * 
+   * @author fkleinoe
+   * @throws Exception e
+   */
+  @Test
+  void testAskForBet() {
+    Player p = new Player("1");
+    p.setId(1);
+    List<Player> after = new ArrayList<Player>();
+    after.add(p);
 
-	@Test
-	void testStartPlay() {
-		fail("Not yet implemented");
-	}
+    assertEquals(after.size(), controller.getPlayer().size());
+    controller.askForBet(18, p);
+    assertEquals(p.getName(), Integer.toString(controller.getPlayer().get(1).getId()));
+  }
 
-	@Test
-	void testAskForBet() {
-		fail("Not yet implemented");
-	}
+  /**
+   * Check if player are added correctly.
+   * 
+   * @author fkleinoe
+   * @throws Exception e
+   */
+  @Test
+  void testReceivedNewBet() {
+    Player p = new Player("1");
+    p.setId(1);
+    List<Player> after = new ArrayList<Player>();
+    after.add(p);
 
-	@Test
-	void testReceivedNewBet() {
-		fail("Not yet implemented");
-	}
+    assertEquals(after.size(), controller.getPlayer().size());
+    controller.askForBet(18, p);
+    assertEquals(p.getName(), Integer.toString(controller.getPlayer().get(1).getId()));
+  }
 
-	@Test
-	void testAskToTakeUpSkat() {
-		fail("Not yet implemented");
-	}
+  /**
+   * Check for different observations by player p playing card c.
+   * 
+   * @author fkleinoe
+   * @throws Exception e
+   */
+  @Test
+  void testReceivedNewCard() {
+    Player partner = new Player("Partner");
+    partner.setId(200);
+    Player opponent = new Player("Opponent");
+    opponent.setId(300);
+    opponent.setDeclarer(true);
+    // Ask for bet just to set correct ids
+    controller.askForBet(18, partner);
+    controller.askForBet(20, opponent);
+    Player[] group = new Player[3];
+    group[0] = bot;
+    group[1] = partner;
+    group[2] = opponent;
+    PlayState playState = new PlayState(group);
+    playState.setPlayMode(PlayMode.GRAND);
+    List<Card> currentTrick = new ArrayList<Card>();
+    currentTrick.add(new Card(Colour.HEARTS, Number.TEN));
+    controller.setCurrentTrick(currentTrick);
+    controller.setExistingTrumps(4);
 
-	@Test
-	void testSwitchSkat() {
-		fail("Not yet implemented");
-	}
+    Card card = new Card(Colour.SPADES, Number.JACK);
+    int colour = 3 - Colour.SPADES.ordinal();
+    int number = 7 - Number.JACK.ordinal();
+    controller.receivedNewCard(card, opponent);
+    // playedCard
+    assertEquals(card, controller.getPlayedCards()[controller.getPlayedCards().length - 1][2]);
 
-	@Test
-	void testAskToSetPlayState() {
-		fail("Not yet implemented");
-	}
+    // Card probability
+    assertEquals(0, controller.getCardProbabilities()[colour * 8 + number][2], 0);
 
-	@Test
-	void testAskToRekontra() {
-		fail("Not yet implemented");
-	}
+    // hasColour
+    int serveColour = 3 - Colour.HEARTS.ordinal();
+    assertFalse(controller.getHasColour()[serveColour][2]);
 
-	@Test
-	void testAskToPlayCard() {
-		fail("Not yet implemented");
-	}
+    // hasTrump
+    assertTrue(controller.getHasTrump()[2]);
+    
+    // existingTrumps
+    assertEquals(3, controller.getExistingTrumps());
+  }
 
-	@Test
-	void testReceivedNewCard() {
-		fail("Not yet implemented");
-	}
+  /**
+   * Check if opponent and partner are set correctly.
+   * 
+   * @author fkleinoe
+   * @throws Exception e
+   */
+  @Test
+  void testSetPlaySettingsAfterAuction() {
+    // Check partner and opponents
+    Player partner = new Player("Partner");
+    partner.setId(200);
+    Player opponent = new Player("Opponent");
+    opponent.setId(300);
+    opponent.setDeclarer(true);
+    Player[] group = new Player[3];
+    group[0] = bot;
+    group[1] = partner;
+    group[2] = opponent;
+    // Ask for bet just to set correct ids
+    controller.askForBet(18, partner);
+    controller.askForBet(20, opponent);
 
-	@Test
-	void testSetPlaySettingsAfterAuction() {
-		fail("Not yet implemented");
-	}
+    PlayState playState = new PlayState(group);
+    playState.setPlayMode(PlayMode.GRAND);
 
-	@Test
-	void testUpdateHand() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testShowScore() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSetGameSettings() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSetCardProbability() {
-		fail("Not yet implemented");
-	}
+    controller.setPlaySettingsAfterAuction(playState);
+    assertEquals(Integer.toString(partner.getId()), controller.getPartner().getName());
+  }
 
 }
