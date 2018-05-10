442a3cd5997a50887eca41cf1ce7a375792b2247
Sandra Beate Angelika Fischer <sandfisc@mail.uni-mannheim.de>
2018-05-10 02:35:59 +0200

    #11 ...

--- src/database/PngReader.java
+++ src/database/PngReader.java
@@ -3,10 +3,57 @@
  */
 package database;
 
-/**
- * @author sandr
- *
- */
-public class PngReader {
+import java.util.ArrayList;
+import java.util.List;
+import logic.Colour;
+import logic.Number;
 
+public class PngReader {
+  
+  String[] colours;
+  String[] numbers;
+  String [] darkLight;
+  
+  public PngReader() {   
+    this.fillColours();
+    this.fillNumbers();
+    this.fillDarkLight();
+  }
+  
+  public void fillColours() {
+    this.colours = new String[4];
+    this.colours[0] = "diamonds";
+    this.colours[1] = "hearts";
+    this.colours[2] = "spades";
+    this.colours[3] = "clubs";
+  }
+  
+  public void fillNumbers() {
+    this.numbers = new String[8];
+    this.numbers[0] = "seven";
+    this.numbers[1] = "eight";
+    this.numbers[2] = "nine";
+    this.numbers[3] = "ten";
+    this.numbers[4] = "jack";
+    this.numbers[5] = "queen";
+    this.numbers[6] = "king";
+    this.numbers[7] = "ass";
+  }
+  
+  public void fillDarkLight() {
+    this.darkLight = new String[2];
+    this.darkLight[0] = "dark";
+    this.darkLight[1] = "light";
+  }
+  
+//  public List<CardImage> readAll(){
+//    List<CardImage> cards = new ArrayList<CardImage>();
+//    for (String colour : this.colours) {
+//      for (String number : this.numbers) {
+//        for (String darkLight : this.darkLight) {
+//          
+//        }
+//      }
+//    }
+//  }
 }
